680a8f44db3caacc50df3fb1f8ec8f543b95ff97
Abhishek <avisekbhumi586@gmail.com>
2018-12-13 15:45:02 +0530

    Add existing file

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CrudproductComponent } from './crudproduct/crudproduct.component';
+import { CruddoctorComponent } from './cruddoctor/cruddoctor.component';
+import { FormsModule } from '@angular/forms';
+import { AssetcomponentsComponent } from './assetcomponents/assetcomponents.component';
+import { EmployeeComponent } from './employee/employee.component';
+import {HttpModule} from '@angular/http';
+
+import {NgxPaginationModule} from 'ngx-pagination';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CrudproductComponent,
+    CruddoctorComponent,
+    AssetcomponentsComponent,
+    EmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    NgxPaginationModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
